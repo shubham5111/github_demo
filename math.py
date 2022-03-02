65c74886aa531f00394b7175aa44337a1c0a768a
shubham5111 <100762141+shubham5111@users.noreply.github.com>
2022-03-02 16:19:38 +0530

    initial commit

---
+++ math.py
@@ -0,0 +1,21 @@
+#Add implementation;
+def add(x,y):
+	return x+y
+# Subtract implementation
+def substract(x,y):
+	a=subtract 		# bug456
+	return x-y
+# Multiple implementation
+def multiply(x,y):
+	return x*y     #bug 456 branch
+# Divide implementation
+def divide(x,y):
+	return x/y	# master branch
+<<<<<<< HEAD
+	if y==0:		# on bug 789
+		return divide by 0_error
+	else :
+	return x/y 
+# square implementation
+def square(x) :
+	return x*x
